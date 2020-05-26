9d9aadb9239d1231ae312de37fac96e27e58c1ea
Thomas BENARD <thomas0benard@gmail.com>
2020-05-26 17:10:27 +0200

    changed target.value to a number

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,11 +3,13 @@ import React, { useState } from 'react';
 function Counter() {
   // Déclare une nouvelle variable d'état, qu’on va appeler « count »
   // Déclare une fonction setCount qui permet de modifier la valeur de count
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState();
 
   function handleNumberChange(e) {
-    setCount(e.target.value);
-    console.log(setCount)
+    const value = Number(e.target.value);
+    setCount(value);
+    console.log(typeof value, value);
+    console.log(typeof e.target.value, e.target.value);
   }
 
   return (
