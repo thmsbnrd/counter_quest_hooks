c1ae85ece9b89ae5dd65cc9f046e1ff06ba14511
Thomas BENARD <thomas0benard@gmail.com>
2020-07-11 12:19:27 +0200

    inital count value at 0 to fix NaN error

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,31 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Counter() {
   // Déclare une nouvelle variable d'état, qu’on va appeler « count »
   // Déclare une fonction setCount qui permet de modifier la valeur de count
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(0);
 
   function handleNumberChange(e) {
     const value = Number(e.target.value);
     setCount(value);
-    console.log(typeof value, value);
-    console.log(typeof e.target.value, e.target.value);
+    // console.log(typeof value, value);
+    // console.log(typeof e.target.value, e.target.value);
   }
 
   return (
     <div>
-      <input
-          type="number"
-          onChange={handleNumberChange}
-          value={count}
-      />
-      <p>Le compteur est  à : {count} </p>
-      <button onClick={() => setCount(count + 1)}>
-        +1
-      </button>
-      <button onClick={() => setCount(count - 1)}>
-        -1
-      </button>
+      <input type="number" onChange={handleNumberChange} value={count} />
+      <p>Le compteur est à : {count} </p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
     </div>
   );
 }
